18c597d9f1c6f7009908f37766ea5b600546560b
kachireddy2003 <100481988+kachireddy2003@users.noreply.github.com>
2022-04-06 20:45:07 +0530

    simple interst calculation
    simple interest calculation

---
+++ simple interest calculation.c
@@ -0,0 +1,15 @@
+// to calculate the simple interest 
+#include<stdio.h>
+int main()
+{
+	int p,t,s;
+	float r;
+	p=1000;
+	t=209;
+	r=5;
+	s=p*t*r/100;
+	printf("%d",s);
+}
+// in antor way we ask to user to input th data 
+// printf(" enter the p,t,r,");
+//scanf("%d%d%d,&p,&t,&r");
