6309be5aef40a3366ac94042bc9150d150ff6ac2
kachireddy2003 <100481988+kachireddy2003@users.noreply.github.com>
2022-04-09 09:55:04 +0530

    // tp print the diffrent types pattern pring programs  // if you want same line diffrent values print the j and same line same values print the i and print star print the star // program // for any programing launagurue this is the  formate + //formate  //for(i=1;i<=n;i++);  //{  //	for(j=1;j<=i;j++);  //	{  //		printf("%d",i,j,*,);// as your wish //	 } //	 printf("  \n");  //}  //            --------program-------  #include<stdio.h>  int main()  {  	int n,i,j;  	printf("enter the n value ");  	scanf("%d",&n);  	for(i=1;i<=n;i++)  	{  		for(j=1;j<=i;j++)  		{  			printf("%d",i); 		 } 		 printf("  \n"); 	 }  	  }
    // tp print the diffrent types pattern pring programs 
    // if you want same line diffrent values print the j and same line same values print the i and print star print the star
    // program // for any programing launagurue this is the  formate +
    //formate
     //for(i=1;i<=n;i++);
     //{
     //	for(j=1;j<=i;j++);
     //	{
     //		printf("%d",i,j,*,);// as your wish
    //	 }
    //	 printf("  \n");
     //}
     //            --------program-------
     #include<stdio.h>
     int main()
     {
     	int n,i,j;
     	printf("enter the n value ");
     	scanf("%d",&n);
     	for(i=1;i<=n;i++)
     	{
     		for(j=1;j<=i;j++)
     		{
     			printf("%d",i);
    		 }
    		 printf("  \n");
    	 }
     	
     }

---
+++ payttern printing programs types and modes  3 tyes.c
@@ -0,0 +1,30 @@
+// tp print the diffrent types pattern pring programs 
+// if you want same line diffrent values print the j and same line same values print the i and print star print the star
+// program // for any programing launagurue this is the  formate +
+//formate
+ //for(i=1;i<=n;i++);
+ //{
+ //	for(j=1;j<=i;j++);
+ //	{
+ //		printf("%d",i,j,*,);// as your wish
+//	 }
+//	 printf("  \n");
+ //}
+ //            --------program-------
+ #include<stdio.h>
+ int main()
+ {
+ 	int n,i,j;
+ 	printf("enter the n value ");
+ 	scanf("%d",&n);
+ 	for(i=1;i<=n;i++)
+ 	{
+ 		for(j=1;j<=i;j++)
+ 		{
+ 			printf("     kar    ");
+		 }
+		 printf("  \n");
+	 }
+ 	
+ }
+ 
